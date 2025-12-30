359c698aec28a49613a2bf583977355fc9012ba9
Suman Rana <sumanrana2062@gmail.com>
2025-12-30 22:08:33 +0545

    forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS if explicitly set in env or if APP_URL uses https
+        $forceHttps = env('FORCE_HTTPS', false);
+        $appUrl = env('APP_URL', 'http://localhost');
+        
+        if ($forceHttps || str_starts_with($appUrl, 'https://')) {
+            URL::forceScheme('https');
+        }
     }
 }
